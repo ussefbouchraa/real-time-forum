9625fcdef2f1b7a4bde1a48a5e8d9214728fa47a
ussef <ussefbouchraa@gmail.com>
2025-09-07 17:58:34 +0100

    feat: setup the server and config file

--- modules/chat/chat_handler.go
+++ modules/chat/chat_handler.go
@@ -1,1 +1,10 @@
 package chat
+
+import (
+	"net/http"
+)
+
+func ChatWebSocketHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement websocket logic
+	w.Write([]byte("WebSocket Endpoint"))
+}
--- server/main.go
+++ server/main.go
@@ -1,1 +1,64 @@
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"real-time-forum/modules/auth"
+	"real-time-forum/modules/posts"
+	"real-time-forum/modules/chat"
+	"real-time-forum/modules/core"
+
+)
+
+
+func mainHandler(w http.ResponseWriter, r *http.Request){
+if r.URL.Path == "/" && r.Method == http.MethodGet {
+	tmpl, err := template.ParseGlob("static/*.html")
+	// tmpl, err := template.ParseFiles("static/index.html")
+	if err != nil { http.Error(w, "Internal Server Error", http.StatusInternalServerError); return}
+	tmpl.Execute(w, nil)
+	
+}else if strings.HasPrefix(r.URL.Path, "/static"){
+	switch r.URL.Path{
+		case "/static/style.css"  :
+			http.StripPrefix("/static/",http.FileServer(http.Dir("static"))).ServeHTTP(w,r) 
+		default :
+			http.Error(w, "Forbiden Page", http.StatusForbidden)
+	}
+			
+}else{	http.Error(w, "Page Not Found", http.StatusNotFound)}
+
+}
+
+func main(){
+	args := os.Args[1: ]
+	if len(args) != 0 { return }
+	
+
+	// Read DB path from config.
+	// Open a connection to SQLite (or other DB).
+	// Optionally run migrations to create tables if they don’t exist.
+	
+    // Load config + Init DB
+    core.InitDB(core.LoadConfig().DatabasePath)
+
+
+
+	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/login", auth.LoginHandler)
+	http.HandleFunc("/register", auth.RegisterHandler)
+	http.HandleFunc("/posts", posts.PostsHandler)
+	http.HandleFunc("/ws/chat", chat.ChatWebSocketHandler)
+
+
+	fmt.Println("Server started at http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Fatal("ListenAndServe:", err)
+	}
+
+
+}
