b4da8ba7bcc7e4b06aecd2fa76f2a0ee61b8d543
ussef <ussefbouchraa@gmail.com>
2025-09-10 18:45:03 +0100

    feat: Create migration tables for the DB

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module real-time-forum
 
-go 1.22.3
+go 1.23
 
 require (
 	github.com/01-edu/z01 v0.1.0 // indirect
--- modules/core/database.go
+++ modules/core/database.go
@@ -16,7 +16,7 @@ func InitDB(path string) {
         log.Fatal("Failed to connect database:", err)
     }
 
-    // createTables()
+    createTables()
 }
 
 func GetDB() *sql.DB {
@@ -24,22 +24,78 @@ func GetDB() *sql.DB {
 }
 
 // Optional: migrations
+func createTables() {
+    userTable()
+    categoryTable()
+    postTable()
+    commentTable()
+    postCategoryTable()
+}
 
-// func createTables() {
-//     query := `
-//     CREATE TABLE IF NOT EXISTS users (
-//         id INTEGER PRIMARY KEY AUTOINCREMENT,
-//         nickname TEXT UNIQUE,
-//         email TEXT UNIQUE,
-//         password TEXT,
-//         age INTEGER,
-//         gender TEXT,
-//         first_name TEXT,
-//         last_name TEXT
-//     );
-//     `
-//     _, err := db.Exec(query)
-//     if err != nil {
-//         log.Fatal("Failed to run migrations:", err)
-//     }
-// }
+func userTable() {
+    query := `CREATE TABLE IF NOT EXISTS users (
+        user_id TEXT PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        nickname TEXT UNIQUE,
+        age SMALLINT,
+        email TEXT UNIQUE,
+        password TEXT
+    )`
+    if _, err := GetDB().Exec(query); err != nil {
+        log.Fatal("Failed to run migrations:", err)
+    }
+}
+
+func categoryTable() {
+    query := `CREATE TABLE IF NOT EXISTS category (
+        category_id TEXT PRIMARY KEY,
+        category_name TEXT NOT NULL
+    )`
+    if _, err := GetDB().Exec(query); err != nil {
+        log.Fatal("Failed to run migrations:", err)
+    }
+}
+
+func postTable() {
+    query := `CREATE TABLE IF NOT EXISTS post (
+        post_id TEXT PRIMARY KEY,
+        content TEXT NOT NULL,
+        created_at TIMESTAMP NOT NULL,
+        post_reaction INTEGER,
+        user_id TEXT,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+    )`
+    if _, err := GetDB().Exec(query); err != nil {
+        log.Fatal("Failed to run migrations:", err)
+    }
+}
+
+func commentTable() {
+    query := `CREATE TABLE IF NOT EXISTS comment (
+        comment_id TEXT PRIMARY KEY,
+        content TEXT NOT NULL,
+        created_at TIMESTAMP NOT NULL,
+        comment_reaction INTEGER,
+        user_id TEXT,
+        post_id TEXT,
+        FOREIGN KEY (user_id) REFERENCES users(user_id),
+        FOREIGN KEY (post_id) REFERENCES post(post_id)
+    )`
+    if _, err := GetDB().Exec(query); err != nil {
+        log.Fatal("Failed to run migrations:", err)
+    }
+}
+
+func postCategoryTable() {
+    query := `CREATE TABLE IF NOT EXISTS post_category (
+        post_id TEXT,
+        category_id TEXT,
+        PRIMARY KEY (post_id, category_id),
+        FOREIGN KEY (post_id) REFERENCES post(post_id),
+        FOREIGN KEY (category_id) REFERENCES category(category_id)
+    )`
+    if _, err := GetDB().Exec(query); err != nil {
+        log.Fatal("Failed to run migrations:", err)
+    }
+}
